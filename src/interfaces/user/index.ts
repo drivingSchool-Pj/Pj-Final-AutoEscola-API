8eaa6b2b4027e19c62ba8b6f2a38f8bf8a7d03a2
GiovannaAlonso <106785638+Alonsogio@users.noreply.github.com>
2023-01-11 12:47:37 -0300

    add postRegister

--- src/interfaces/user/index.ts
+++ src/interfaces/user/index.ts
@@ -1,1 +1,13 @@
-123
+export interface IUserRequest {
+  id: string;
+  name: string;
+  email: string;
+  contact: number;
+  age: number;
+  cpf: string;
+  isAdm: boolean;
+  isActive: boolean;
+  typeCategorie: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
--- src/validations/schemas.ts
+++ src/validations/schemas.ts
@@ -1,1 +1,19 @@
-import * as yup from 'yup';
+import * as yup from "yup";
+import { IUserRequest } from "../interfaces/user";
+import { SchemaOf } from "yup";
+
+export const userWithoutPasswordValidation: SchemaOf<IUserRequest> = yup
+  .object()
+  .shape({
+    id: yup.string().notRequired(),
+    name: yup.string().notRequired(),
+    email: yup.string().email().notRequired(),
+    contact: yup.number().notRequired(),
+    age: yup.number().notRequired(),
+    cpf: yup.string().notRequired(),
+    isAdm: yup.boolean().notRequired(),
+    isActive: yup.boolean().notRequired(),
+    typeCategorie: yup.string().notRequired(),
+    createdAt: yup.date().notRequired(),
+    updatedAt: yup.date().notRequired(),
+  });
