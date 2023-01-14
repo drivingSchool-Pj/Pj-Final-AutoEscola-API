163094567f64f6979a7a4a56d869850e246ee698
BRENO LOBIANCO DOS SANTOS <breno.lobianco@gmail.com>
2023-01-14 14:36:13 -0300

    feat:list location by id

---
+++ src/services/location/listLocationById.service.ts
@@ -0,0 +1,12 @@
+import AppDataSource from "../../data-source";
+import { Location } from "../../entities/location.entity";
+
+export const listLocationDataService = async (locationId: string) => {
+  const locationRepository = AppDataSource.getRepository(Location);
+
+  const locationData = await locationRepository.findOneBy({
+    id: locationId,
+  });
+
+  return locationData;
+};
