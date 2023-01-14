5498ab36758e17417a7336f6c6a877928cc8cec0
BRENO LOBIANCO DOS SANTOS <breno.lobianco@gmail.com>
2023-01-14 03:27:55 -0300

    feat:delete category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import { softDeleteCategoryController } from "../controllers/categories/deleteCategories,controller";
 
 const categoriesRoutes = Router()
 
+categoriesRoutes.delete("/:id", softDeleteCategoryController);
 
 export default categoriesRoutes
