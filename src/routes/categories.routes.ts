e9b9a625413b3e21ef2cfdd470baa5c1301246bf
Leonardo Souza <leonardobennr@gmail.com>
2023-01-14 13:47:39 -0300

    feat: remove delete for categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,5 @@
 import { Router } from "express";
-import { softDeleteCategoryController } from "../controllers/categories/deleteCategories,controller";
-import ensureIsAdmMiddleware from "../middlewares/ensureVerifyIsAdm.middleware";
 
-const categoriesRoutes = Router()
+const categoriesRoutes = Router();
 
-categoriesRoutes.delete("/:id",ensureIsAdmMiddleware, softDeleteCategoryController);
-
-export default categoriesRoutes
+export default categoriesRoutes;
