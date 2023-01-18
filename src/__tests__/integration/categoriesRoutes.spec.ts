d6e4198998b2dd153f93ef6edf5ff1531f46debf
Daniel Hermenegildo Gimenez <attackhu3@gmail.com>
2023-01-18 10:43:36 -0300

    fix/settingsTestsCategories finalized

--- src/__tests__/integration/categoriesRoutes.spec.ts
+++ src/__tests__/integration/categoriesRoutes.spec.ts
@@ -1,76 +1,71 @@
-import { DataSource } from "typeorm";
-import AppDataSource from "../../data-source";
-import request from "supertest";
-import app from "../../app";
+import { DataSource } from 'typeorm'
+import AppDataSource from '../../data-source'
+import request from 'supertest'
+import app from '../../app'
 import {
   mockedAdmin,
   mockedAdminLogin,
+  mockedInstructor,
   mockedUser,
-  mockedUserLogin,
-} from "../mocks/index";
+  mockedUserLogin
+} from '../mocks/createUser'
+import { Categories } from '../../entities/categories.entity'
 
-describe("/categories routes", () => {
-  let connection: DataSource;
-
+describe('/categories routes', () => {
+  let connection: DataSource
+  const categoryRepo = AppDataSource.getRepository(Categories);
   beforeAll(async () => {
     await AppDataSource.initialize()
-      .then((res) => {
-        connection = res;
+      .then(res => {
+        connection = res
       })
-      .catch((err) => {
-        console.error("Error during Data Source initialization", err);
-      });
-
-    await request(app).post("/users").send(mockedUser);
-    await request(app).post("/users").send(mockedAdmin);
-  });
+      .catch(err => {
+        console.error('Error during Data Source initialization', err)
+      })
+  })
 
   afterAll(async () => {
-    await connection.destroy();
-  });
+    await connection.destroy()
+  })
 
-  test("GET /categories/instructor/:id -  must be able to list the categories of an instructor", async () => {
+  test('GET /categories/instructor/:id -  must be able to list the categories of an instructor', async () => {
+    const category = categoryRepo.create({typeCategorie: "A"});
+    await categoryRepo.save(category);
+    const userBody:any = await request(app).post('/user').send(mockedUser)
+    await request(app).post('/user').send(mockedAdmin)
+    
     const adminLoginResponse = await request(app)
-      .post("/login")
-      .send(mockedAdminLogin);
-    const instructor = await request(app).get("/instructor");
-    const response = await request(app)
-      .get(`/categories/instructor/${instructor.body[0].id}`)
-      .set("Authorization", `Bearer ${adminLoginResponse.body.token}`);
+      .post('/login')
+      .send(mockedAdminLogin)
+    
+    const response = await request(app).post('/instructors').send({id: userBody.body.id, category: "A"})
+    .set('Authorization', `Bearer ${adminLoginResponse.body.token}`)
+  
+    expect(response.body.message).toBe("Instructor created successfully")
 
-    expect(response.body).toHaveProperty("categories");
-    expect(response.body.schedules[0]).toHaveProperty("id");
-    expect(response.body.schedules[0]).toHaveProperty("name");
+    expect(response.status).toBe(200)
+  })
 
-    expect(response.body.schedules).toHaveLength(1);
-    expect(response.status).toBe(200);
-  });
-  test("GET /categories/instructor/:id -  should not be able to list the categories of a instructor without authentication", async () => {
-    const instructor = await request(app).get("/instructor");
+  test('GET /categories/instructors/:id -  should not be able to list the categories of a instructor without authentication', async () => {
+    const instructor = await request(app).get('/instructors')
     const response = await request(app).get(
-      `/categories/instructor/${instructor.body[0].id}`
-    );
+      `/categories/instructors/${instructor.body[0].id}`
+    )
 
-    expect(response.body).toHaveProperty("message");
-    expect(response.status).toBe(401);
-  });
+    expect(response.body).toHaveProperty('message')
+    expect(response.status).toBe(401)
+  })
 
-  test("GET /categories/instructor/:id -  should not be able to list the categories of a instructor not being admin", async () => {
+  test('GET /categories/instructor/:id -  should not be able to list the categories of a instructor not being admin', async () => {
     const userLoginResponse = await request(app)
-      .post("/login")
-      .send(mockedUserLogin);
-    const instructor = await request(app).get("/instructor");
+      .post('/login')
+      .send(mockedUserLogin)
+    const instructor = await request(app).get('/instructors')
     const response = await request(app)
-      .get(`/categories/instructor/${instructor.body[0].id}`)
-      .set("Authorization", `Bearer ${userLoginResponse.body.token}`);
+      .get(`/categories/instructors/${instructor.body[0].id}`)
+      .set('Authorization', `Bearer ${userLoginResponse.body.token}`)
 
-    expect(response.body).toHaveProperty("message");
-    expect(response.status).toBe(403);
-  });
-
-  test("GET /categories -  Must be able to list all categories", async () => {
-    const response = await request(app).get("/categories");
-    expect(response.body).toHaveLength(1);
-    expect(response.status).toBe(200);
-  });
-});
+    expect(response.body).toHaveProperty('message')
+    expect(response.status).toBe(403)
+  })
+})
